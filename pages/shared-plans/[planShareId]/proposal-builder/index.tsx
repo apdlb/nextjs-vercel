471a6fd68189910feec3fab6009d581201c69e9d
Alejandro Páez <alepdlb@gmail.com>
2022-04-05 09:56:20 +0200

    feat: add dynamic route

---
+++ pages/shared-plans/[planShareId]/proposal-builder/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const ProposalBuilderPage: NextPage = () => (
+  <>
+    <Head>
+      <title>Proposal Builder</title>
+    </Head>
+    <div>Proposal Builder</div>
+  </>
+);
+
+export default ProposalBuilderPage;
